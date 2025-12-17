4a96f30cfa47e1680228d8f276eb9b509ec7943b
Patrick2oo3 <theecopatoh@gmail.com>
2025-12-17 22:48:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,20 @@ setTimeout(summonSanta, 3000);
 
 // ...and then every 2 minutes
 setInterval(summonSanta, 120000);
+
+function updateCountdown() {
+    const nextYear = new Date('January 1, 2026 00:00:00').getTime();
+    const now = new Date().getTime();
+    const diff = nextYear - now;
+
+    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = d.toString().padStart(2, '0');
+    document.getElementById('hours').innerText = h.toString().padStart(2, '0');
+    document.getElementById('minutes').innerText = m.toString().padStart(2, '0');
+    document.getElementById('seconds').innerText = s.toString().padStart(2, '0');
+}
+setInterval(updateCountdown, 1000);
